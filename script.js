79ae8f5342b41b8e5d9474c481bff4b54a6cab2a
Sam12604 <42171911+Sam12604@users.noreply.github.com>
2022-03-18 19:32:14 +0000

    yoo

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 async function getData() {
-    response1 = await fetch('https://jsonkeeper.com/b/DVXV');
-    api_data1 = await response1.json()
-    console.log(api_data1)
+    response1 = await fetch('https://sam12604.github.io/drivingtheorywebsite/questions.json');
+    questions_data = await response1.json()
+
+    i = 1;
+
+    document.getElementById("current_question").innerText = questions_data[i].question
+    document.getElementById("answer1").innerText = questions_data[i].tans
+    document.getElementById("answer2").innerText = questions_data[i].ans1
+    document.getElementById("answer3").innerText = questions_data[i].ans2
+    document.getElementById("answer4").innerText = questions_data[i].ans3
+
+    if (questions_data[i].diagram == true) {
+        document.getElementById("diagram_img").src = "files/images/" + questions_data[i].ref + ".jpg"
+    }
 }
 
 getData(); //https://jsonkeeper.com/b/WZO5
