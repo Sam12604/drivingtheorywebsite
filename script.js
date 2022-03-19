01189301f7e0f45ee6aee229073b343a84382de7
Sam12604 <42171911+Sam12604@users.noreply.github.com>
2022-03-19 15:03:34 +0000

    yo mum

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 async function getData() {
     response1 = await fetch('https://sam12604.github.io/drivingtheorywebsite/questions.json');
-    questions_data = await response1.json()
+    questions_data = await response1.json();
+    loadQuestion()
+}
 
-    i = 1;
+function loadQuestion() {
+    let i = Math.floor((Math.random() * questions_data.length) + 1) - 1;
+    console.log(i);
 
-    document.getElementById("current_question").innerText = questions_data[i].question
-    document.getElementById("answer1").innerText = questions_data[i].tans
-    document.getElementById("answer2").innerText = questions_data[i].ans1
-    document.getElementById("answer3").innerText = questions_data[i].ans2
-    document.getElementById("answer4").innerText = questions_data[i].ans3
+    document.getElementById("current_question").innerText = questions_data[i].question;
+    document.getElementById("answer1").innerText = questions_data[i].tans;
+    document.getElementById("answer2").innerText = questions_data[i].ans1;
+    document.getElementById("answer3").innerText = questions_data[i].ans2;
+    document.getElementById("answer4").innerText = questions_data[i].ans3;
 
     if (questions_data[i].diagram == true) {
-        document.getElementById("diagram_img").src = "files/images/" + questions_data[i].ref + ".jpg"
+        document.getElementById("diagram_img").src = "files/images/" + questions_data[i].ref + ".jpg";
+        document.getElementById("diagram_img").style.display = "block";
+    } else {
+        document.getElementById("diagram_img").src = "";
+        document.getElementById("diagram_img").style.display = "none";
     }
 }
 
-getData(); //https://jsonkeeper.com/b/WZO5
+getData();
