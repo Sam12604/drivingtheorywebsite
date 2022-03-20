561f834a08849819bae26bf29848e2c19c67afce
Sam12604 <42171911+Sam12604@users.noreply.github.com>
2022-03-20 18:36:23 +0000

    big tings

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 questions = []
-
-//import questions from questions.json file and format to json
+max = 0
+selected_answer = -1
+    //import questions from questions.json file and format to json
 async function getData() {
     questions = []
     response1 = await fetch('https://SimplyDrivingTheory.co/questions.json');
@@ -9,7 +10,41 @@ async function getData() {
         //loads relevent questions, currently all of them but may change if I implement topic picking
         questions.push(questions_data[question].ref);
     }
+    max = questions.length;
+    document.getElementById("progressbar").max = max;
     loadQuestion();
+}
+
+//easily set all answer boxes to one value
+function setAllAnswers(text) {
+    for (x = 1; x <= 4; x++) {
+        x2 = "answer" + x
+        document.getElementById(x2).innerText = text
+    }
+}
+
+//selects active answer
+function selectAnswer(id) {
+    selected_answer = id
+    document.getElementById(id).style = "background-color: rgb(221, 221, 221, 0.6);border-radius: 5px;padding: 8px;"
+        /* rgb(145, 238, 145) green colour code for correct answer */
+    for (x = 1; x <= 4; x++) {
+        x2 = "answer" + x
+
+        if (id != x2) {
+            document.getElementById(x2).style = " "
+        }
+    }
+}
+
+//clears selected answer
+function clearSelected() {
+    selected_answer = -1
+
+    for (x = 1; x <= 4; x++) {
+        x2 = "answer" + x
+        document.getElementById(x2).style = " "
+    }
 }
 
 //loads random question onto screen and puts answers in random order
@@ -25,6 +60,9 @@ function loadQuestion() {
                 index = item;
             }
         }
+
+        //updates progressbar
+        document.getElementById("progressbar").value = (max + 1) - questions.length;
 
         //displays the answers on the screen in a random order
         answer_order = ['tans', 'ans1', 'ans2', 'ans3'];
@@ -50,6 +88,9 @@ function loadQuestion() {
 
         //removes the completed question from the list of avaliable questions
         questions.splice(questions.indexOf(questions_data[index].ref), 1);
+
+        //clears selected answers
+        clearSelected();
     } else {
         //moves to end of quiz screen which will eventually display score
         endOfQuiz();
@@ -58,4 +99,5 @@ function loadQuestion() {
 
 function endOfQuiz() {
     document.getElementById("current_question").innerText = 'out of questions';
+    setAllAnswers()
 }
