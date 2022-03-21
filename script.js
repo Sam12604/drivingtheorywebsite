983d95ec854268fc0c921042ea0ed0ac2010d25e
Sam12604 <42171911+Sam12604@users.noreply.github.com>
2022-03-21 12:24:18 +0000

    let rock and a roll

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 correct = new Audio('files/sounds/juntos-607.mp3');
 questions = []
 max = 0
+score = 0
+correct_answer = ''
+correct_answer_label = '';
 selected_answer = -1
     //import questions from questions.json file and format to json
 async function getData() {
     questions = []
+    score = 0
     response1 = await fetch('https://SimplyDrivingTheory.co/questions.json');
     questions_data = await response1.json();
     for (question = 0; question < questions_data.length; question++) {
@@ -19,37 +23,47 @@ async function getData() {
 //easily set all answer boxes to one value
 function setAllAnswers(text) {
     for (x = 1; x <= 4; x++) {
-        x2 = "answer" + x
-        document.getElementById(x2).innerText = text
+        x2 = "answer" + x;
+        document.getElementById(x2).innerText = text;
+    }
+}
+
+//unchecks all radios
+function uncheck() {
+    for (x = 1; x <= 4; x++) {
+        x2 = "ans" + x;
+        document.getElementById(x2).checked = false;
     }
 }
 
 //selects active answer
 function selectAnswer(id) {
     selected_answer = id
-    document.getElementById(id).style = "background-color: rgb(221, 221, 221, 0.6);border-radius: 5px;padding: 8px;"
-        /* rgb(145, 238, 145) green colour code for correct answer */
+    document.getElementById(id).style = "background-color: rgb(221, 221, 221, 0.6);border-radius: 5px;padding: 8px;";
     for (x = 1; x <= 4; x++) {
-        x2 = "answer" + x
+        x2 = "answer" + x;
 
         if (id != x2) {
-            document.getElementById(x2).style = " "
+            document.getElementById(x2).style = " ";
         }
     }
 }
 
 //clears selected answer
 function clearSelected() {
-    selected_answer = -1
+    selected_answer = -1;
 
     for (x = 1; x <= 4; x++) {
-        x2 = "answer" + x
-        document.getElementById(x2).style = " "
+        x2 = "answer" + x;
+        document.getElementById(x2).style = " ";
     }
 }
 
 //loads random question onto screen and puts answers in random order
 function loadQuestion() {
+    //unchecks seleted radios
+    uncheck()
+
     //makes sure there is questions avaliable
     if (questions.length > 0) {
         //random number from length of avaliable questions
@@ -70,8 +84,18 @@ function loadQuestion() {
         for (let r = 0; r < 4; r++) {
             x = "answer" + String(r + 1);
 
+            //picks random position from those avaliable in answer_order
             rand = Math.floor(Math.random() * answer_order.length);
+
+            //puts answer in text box
             document.getElementById(x).innerText = questions_data[index][answer_order[rand]];
+
+            //saves the position of the correct answer
+            if (answer_order[rand] == "tans") {
+                correct_answer = "ans" + (r + 1);
+                correct_answer_label = "answer" + (r + 1);
+            }
+
             answer_order.splice(rand, 1);
         }
 
@@ -93,17 +117,31 @@ function loadQuestion() {
         //clears selected answers
         clearSelected();
 
-        //MOVE TO FUNCTION THAT CHECKS ANSWER IS CORRECT
-        correct.pause();
-        correct.currentTime = 0;
-        correct.play();
     } else {
         //moves to end of quiz screen which will eventually display score
         endOfQuiz();
     }
 }
 
+function markQuestion() {
+    if (document.getElementById(correct_answer).checked == true) {
+        //plays ding sound
+        correct.play();
+        //makes answer green
+        console.log(correct_answer_label);
+        document.getElementById(correct_answer_label).style = "background-color: rgb(145, 238, 145);border-radius: 5px;padding: 8px;";
+        //adds to score
+        score++
+    } else {
+        //makes answer green if correct
+        selected = document.querySelector('input[name="answer"]:checked').labels[0].id;
+        document.getElementById(selected).style = "background-color: rgb(255, 107, 107);border-radius: 5px;padding: 8px;";
+    }
+}
+
 function endOfQuiz() {
     document.getElementById("current_question").innerText = 'out of questions';
-    setAllAnswers()
+    alert(score);
+    setAllAnswers();
+    clearSelected();
 }
